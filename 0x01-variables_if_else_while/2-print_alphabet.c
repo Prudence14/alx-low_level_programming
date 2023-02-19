0bd8639104902a0d3fa9ecd498a6307396aed0d1
Prudence14 <prudencenkosi1999@gmail.com>
2023-02-19 15:35:51 -0800

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet gam

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
