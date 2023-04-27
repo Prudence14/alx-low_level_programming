5fdb9d0fcea98e1bd09b7b09880c58b9f690a2c8
Prudence14 <prudencenkosi1999@gmail.com>
2023-04-27 09:46:20 -0700

    Tread lightly, she is near

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,14 +1,20 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef main.h
+#define main.h
+
+/*
+ * File: holberton.h
+ * Auth: prudence nkosi 
+ * Desc: Header file contains prototypes for all functions
+ *       written in the 0x15-file_io directory.
+ */
 
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
 
-size_t read_textfile(const char *filename, size_t letters);
+ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
-#endif
-
+#endif /* MAIN_H */
